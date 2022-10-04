d0b61bf9f46189a7f8e4650cfe6ce56260274a33
Jacob Karla <jacob.l.karla@gmail.com>
2022-10-04 16:54:39 -0400

    Removing redundant comments"

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,8 +11,6 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('UPDATE the DB');
   const jateDb = await openDB('jate', 1);
@@ -22,7 +20,6 @@ export const putDb = async (content) => {
   const result = await request;
 }
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the DB');
   const jateDb = await openDB('jate', 1);
